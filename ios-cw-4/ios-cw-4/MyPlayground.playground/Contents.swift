1d0705553d37e82a0ae7e1160e9e496627f1814c
waashii <70151834+waashii@users.noreply.github.com>
2022-03-05 13:41:31 +0300

    done

---
+++ ios-cw-4/ios-cw-4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+var hobby = "day dreaming"
+var name = "عائشة"
+var favouriteNumber = 16
+print("السلام عليكم،معاكم \(name) ورقمي المفضل هو \(favouriteNumber) واحب \(hobby)")
+//السلام عليكم، معاكم اسمك و رقمي المفضل هو  رقمك و احب هوايتك
+
+//\(name)
